4608e103f9b6aa23e32231285976ca7037a9b25e
Lê Xuân Khánh <42823780+lexuankhanh127@users.noreply.github.com>
2025-09-28 12:58:46 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -6,17 +6,17 @@ echo "Restoring manually installed APT software and Flatpak apps"
 sudo apt update
 
 # --- Install APT software ---
-if [ -f apt-software.txt ]; then
-    xargs -a apt-software.txt sudo apt install -y
+if [ -f output/apt-software.txt ]; then
+    xargs -a output/apt-software.txt sudo apt install -y
 else
     echo "apt-software.txt not found!"
 fi
 
 # --- Install Flatpak apps ---
-if [ -f flatpak-apps.txt ]; then
+if [ -f output/flatpak-apps.txt ]; then
     while read -r app; do
         flatpak install -y "$app"
-    done < flatpak-apps.txt
+    done < output/flatpak-apps.txt
 else
     echo "flatpak-apps.txt not found!"
 fi
