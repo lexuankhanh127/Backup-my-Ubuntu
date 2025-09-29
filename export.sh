b8b86f5df01f3bb634a5d1c94fc5950952fe200d
Lê Xuân Khánh <42823780+lexuankhanh127@users.noreply.github.com>
2025-09-29 16:04:46 +0700

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-mkdir -p output
-cd output
 # --- Export Flatpak apps ---
 flatpak list --app --columns=application > flatpak-apps.txt
 echo "Export complete!"
-echo "Flatpak app list saved to: ~/output/flatpak-apps.txt"
